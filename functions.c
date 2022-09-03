d3af50438df89d5ea44b75b67f5e011ae9a11c6a
WemimoAdewale <adewaleadewemimoesther@gmail.com>
2022-09-03 03:18:04 -0700

    included written functions

---
+++ functions.c
@@ -0,0 +1,35 @@
+/**
+* _strlen - find the length of a string
+* @s: pointer to the string to check
+* Return: void
+*/
+
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	return (i);
+}
+
+/**
+* *_strcpy -  copies the string pointed to by src
+* @dest: char type string
+* @src: char type string
+* Description: Copy the string pointed to by poi
+* nter to the buffer
+* Return: Pointer to `dest`
+*/
+char *_strcpy(char *dest, const char *src)
+{
+	int i = -1;
+
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
+
+	return (dest);
+}
