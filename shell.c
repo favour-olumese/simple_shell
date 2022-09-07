db212d5b116517a09075ef24f5282c5173601f09
Favour Olumese <70489864+favour-olumese@users.noreply.github.com>
2022-09-07 21:41:48 +0100

    Reverting back to 9c42bd37d870d69df774a296b4789222f09c3c51.

--- shell.c
+++ shell.c
@@ -1,45 +1,11 @@
 #include "main.h"
 
 /**
- * execute - executes the command
- * @cmd: command to run
- * Return: 0 on success1 -1 if cmd is exit and 1 on any other error
- */
-int execute(char **cmd)
-{
-	pid_t child_pid;
-	int status;
-
-	if (strncmp("exit", cmd[0], 4) == 0)
-		return (-1);
-
-	child_pid = fork();
-
-	if (child_pid == -1)
-	{
-		perror("Error");
-		return (1);
-	}
-	else if (child_pid == 0)
-	{
-		if (execve(cmd[0], cmd, NULL) == -1)
-		{
-			perror("Error");
-			exit(-1);
-		}
-	}
-	else
-		wait(&status);
-
-	return (0);
-}
-/**
-* main - main simple shell
-* @argc: number of arguments
-* @argv: list of command line arguments
-* Return:on success 0
+* main - main simple shell.
+* @argc: number of arguments.
+* @argv: list of command line arguments.
+* Return: Always 0, -1 on error.
 */
-
 int main(int argc, char **argv)
 {
 	int response;
@@ -47,6 +13,7 @@ int main(int argc, char **argv)
 	size_t bufsize = BUFSIZ;
 	int isPipe = 0;
 	char *buffer;
+	ssize_t getline_num;
 
 	if (argc >= 2)
 	{
@@ -57,17 +24,29 @@ int main(int argc, char **argv)
 		}
 		return (0);
 	}
+
+	buffer = (char *)malloc(bufsize * sizeof(char));
+	if (buffer == NULL)
+	{
+		perror("Error: Unable to allocate buffer");
+		exit (1);
+	}
 	do {
 		if (isatty(fileno(stdin)))
 		{
 			isPipe = 1;
-			_puts("cisfun#: ");
+			_puts("$ ");
 		}
+		getline_num = getline(&buffer, &bufsize, stdin);
 
-		getline(&buffer, &bufsize, stdin);
-		buffer[_strlen(buffer) - 1] = '\0';
-		tokens = stringToTokens(buffer);
-		response = execute(tokens);
+		if (getline_num == -1)
+		{
+			exit(EXIT_FAILURE);
+		}
+	buffer[_strlen(buffer) - 1] = '\0';
+	tokens = stringToTokens(buffer);
+	response = execute(tokens);
+
 	} while (isPipe && response != -1);
 
 	return (0);
